841609b619dea6c31b121706d356ae2519d71b09
Tim Crawford <tcrawford@datto.com>
2016-03-10 16:51:57 -0500

    Bump version

--- src/dattobd.c
+++ src/dattobd.c
@@ -19,7 +19,7 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Tom Caputi");
 MODULE_DESCRIPTION("Kernel module for supporting block device snapshots and incremental backups.");
 
-#define VERSION_STRING "0.9.1"
+#define VERSION_STRING "0.9.2"
 MODULE_VERSION(VERSION_STRING);
 
 /*********************************REDEFINED FUNCTIONS*******************************/
