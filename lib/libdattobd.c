2e681e82ed7ac437a069c6e13e5ead77cc0f7bd9
Tom Caputi <tcaputi@datto.com>
2016-03-29 10:35:23 -0400

    fixed whitespace in libdattobd.c

--- lib/libdattobd.c
+++ lib/libdattobd.c
@@ -3,7 +3,7 @@
 
     This file is part of dattobd.
 
-    This program is free software; you can redistribute it and/or modify it 
+    This program is free software; you can redistribute it and/or modify it
     under the terms of the GNU General Public License version 2 as published
     by the Free Software Foundation.
 */
@@ -21,18 +21,18 @@ extern "C" {
 int setup_snapshot(unsigned int minor, char *bdev, char *cow, unsigned long fallocated_space, unsigned long cache_size){
 	int fd, ret;
 	struct setup_params sp;
-	
+
 	fd = open("/dev/datto-ctl", O_RDONLY);
 	if(fd < 0) return -1;
-	
+
 	sp.minor = minor;
 	sp.bdev = bdev;
 	sp.cow = cow;
 	sp.fallocated_space = fallocated_space;
 	sp.cache_size = cache_size;
-	
+
 	ret = ioctl(fd, IOCTL_SETUP_SNAP, &sp);
-	
+
 	close(fd);
 	return ret;
 }
@@ -40,17 +40,17 @@ int setup_snapshot(unsigned int minor, char *bdev, char *cow, unsigned long fall
 int reload_snapshot(unsigned int minor, char *bdev, char *cow, unsigned long cache_size){
 	int fd, ret;
 	struct reload_params rp;
-	
+
 	fd = open("/dev/datto-ctl", O_RDONLY);
 	if(fd < 0) return -1;
-	
+
 	rp.minor = minor;
 	rp.bdev = bdev;
 	rp.cow = cow;
 	rp.cache_size = cache_size;
-	
+
 	ret = ioctl(fd, IOCTL_RELOAD_SNAP, &rp);
-	
+
 	close(fd);
 	return ret;
 }
@@ -58,41 +58,41 @@ int reload_snapshot(unsigned int minor, char *bdev, char *cow, unsigned long cac
 int reload_incremental(unsigned int minor, char *bdev, char *cow, unsigned long cache_size){
 	int fd, ret;
 	struct reload_params rp;
-	
+
 	fd = open("/dev/datto-ctl", O_RDONLY);
 	if(fd < 0) return -1;
-	
+
 	rp.minor = minor;
 	rp.bdev = bdev;
 	rp.cow = cow;
 	rp.cache_size = cache_size;
-	
+
 	ret = ioctl(fd, IOCTL_RELOAD_INC, &rp);
-	
+
 	close(fd);
 	return ret;
 }
 
 int destroy(unsigned int minor){
 	int fd, ret;
-	
+
 	fd = open("/dev/datto-ctl", O_RDONLY);
 	if(fd < 0) return -1;
-	
+
 	ret = ioctl(fd, IOCTL_DESTROY, &minor);
-	
+
 	close(fd);
 	return ret;
 }
 
 int transition_incremental(unsigned int minor){
 	int fd, ret;
-	
+
 	fd = open("/dev/datto-ctl", O_RDONLY);
 	if(fd < 0) return -1;
-	
+
 	ret = ioctl(fd, IOCTL_TRANSITION_INC, &minor);
-	
+
 	close(fd);
 	return ret;
 }
@@ -100,16 +100,16 @@ int transition_incremental(unsigned int minor){
 int transition_snapshot(unsigned int minor, char *cow, unsigned long fallocated_space){
 	int fd, ret;
 	struct transition_snap_params tp;
-	
+
 	tp.minor = minor;
 	tp.cow = cow;
 	tp.fallocated_space = fallocated_space;
-	
+
 	fd = open("/dev/datto-ctl", O_RDONLY);
 	if(fd < 0) return -1;
-	
+
 	ret = ioctl(fd, IOCTL_TRANSITION_SNAP, &tp);
-	
+
 	close(fd);
 	return ret;
 }
@@ -117,34 +117,34 @@ int transition_snapshot(unsigned int minor, char *cow, unsigned long fallocated_
 int reconfigure(unsigned int minor, unsigned long cache_size){
 	int fd, ret;
 	struct reconfigure_params rp;
-	
+
 	fd = open("/dev/datto-ctl", O_RDONLY);
 	if(fd < 0) return -1;
-	
+
 	rp.minor = minor;
 	rp.cache_size = cache_size;
-	
+
 	ret = ioctl(fd, IOCTL_RECONFIGURE, &rp);
-	
+
 	close(fd);
 	return ret;
 }
 
 int dattobd_info(unsigned int minor, struct dattobd_info *info){
 	int fd, ret;
-	
+
 	if(!info){
 		errno = EINVAL;
 		return -1;
 	}
-	
+
 	fd = open("/dev/datto-ctl", O_RDONLY);
 	if(fd < 0) return -1;
-	
+
 	info->minor = minor;
-	
+
 	ret = ioctl(fd, IOCTL_DATTOBD_INFO, info);
-	
+
 	close(fd);
 	return ret;
 }
