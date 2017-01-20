d8a29e717455a1924568fc43fa09d30648b0a2c7
Stu Mark <smark@datto.com>
2017-01-20 17:11:28 -0500

    added support for sle 11 sp2

--- dist/initramfs/initrd/setup-dattobd.sh
+++ dist/initramfs/initrd/setup-dattobd.sh
@@ -5,7 +5,9 @@
 # when the boot-dattobd.sh script runs so it can run reload
 
 export MOUNTROOT=$tmp_mnt/etc/datto/dla/mnt
-
 echo "datto dlad install making mountpoint directory $MOUNTROOT" > /dev/kmsg
 mkdir -p $MOUNTROOT
 cp /var/lib/datto/dla/reload $tmp_mnt/sbin/datto_reload
+
+mkdir -p $tmp_mnt/usr/bin
+mkdir -p $tmp_mnt/usr/sbin
