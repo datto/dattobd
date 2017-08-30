d49f7cb0864a23895db5ba79edd44fbe6d21fec3
Tim Crawford <tcrawford@datto.com>
2017-08-30 15:56:14 -0400

    Linux 4.13 compatibility
    Resolves #106
    
    Signed-off-by: Tim Crawford <tcrawford@datto.com>

---
+++ src/configure-tests/feature-tests/bioset_need_bvecs_flag.c
@@ -0,0 +1,16 @@
+/*
+    Copyright (C) 2017 Datto Inc.
+
+    This file is part of dattobd.
+
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License version 2 as published
+    by the Free Software Foundation.
+*/
+
+#include "../../includes.h"
+
+static inline void dummy(void){
+	int flags = BIOSET_NEED_BVECS;
+	(void)bioset_create(0, 0, flags);
+}
