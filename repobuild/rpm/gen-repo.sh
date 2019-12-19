01a113c97178a0cb841805e6bd42b6f50dfa761d
e-kov <55983595+e-kov@users.noreply.github.com>
2019-12-19 14:41:09 +0200

    Multiple improvements for the CentOS repository build
    * Multiple improvements for the CentOS repository build
    
    1) Fixed build conditions in the yaml script to avoid packages upload
    in the PR builds.
    Tabulation in the .drone.yml was wrong. "when" word was related to
    the build step settings, but not to the build step.
    
    2) Added error handling to the rpm repo build script.
    
    3) Added builds for CentOS 7 and 8 in addition to existing build of CentOS 6.
    
    4) Used fancy repo domain name now repo.assur.io over https.

--- repobuild/rpm/gen-repo.sh
+++ repobuild/rpm/gen-repo.sh
@@ -9,10 +9,12 @@ die () {
 
 dir=$(readlink -f $(dirname $0))
 out=$dir/repo
-cent6=$out/CentOS/6
-pool=$cent6/x86_64/Packages
+dist_ver=`cat /etc/redhat-release | tr -cd [0-9.] | cut -d'.' -f1`
+cent=$out/CentOS/$dist_ver/x86_64
+pool=$cent/Packages
+mirrors=$out/CentOS/mirrors
 branch=`git rev-parse --abbrev-ref HEAD`
-bucket=ci.assur.io
+repo_link=https://repo.assur.io/assurio-snap
 
 [ -z "$GPG_KEY_ID" ]   && die "Env var GPG_KEY_ID is not set"
 [ -z "$GPG_PUB_KEY" ]  && die "Env var GPG_PUB_KEY is not set"
@@ -26,9 +28,9 @@ gpg2 --list-secret-key $GPG_KEY_ID >/dev/null || \
      die "Failed to import gpg key"
 
 # Cleanup and prepare dir structure
-rm -rf $out
-mkdir -p $cent6/x86_64/{Packages,repodata}
-mkdir -p $out/CentOS/mirrors
+rm -rf $cent $mirrors/$dist_ver*
+mkdir -p $cent/{Packages,repodata}
+mkdir -p $mirrors
 
 # Prepare 'Packages' dir. It's analog of the 'pool' dir for deb
 cp -r $dir/../../pkgbuild/RPMS/x86_64/* $pool
@@ -39,18 +41,21 @@ cp -r $dir/../../pkgbuild/SRPMS/*       $pool
 echo "%_gpg_name Assurio Software Package Developers <packages@assurio.com>" >> ~/.rpmmacros
 
 #Sign packages
-find $pool -type f -name "*.rpm" -exec $dir/rpm-sign.exp {} \;
+for f in `ls $pool/*.rpm`; do
+    $dir/rpm-sign.exp $f || die "Failed to sign rpm"
+done
 
 # Generate repodata/repomd.xml
-createrepo $cent6/x86_64
+createrepo $cent || die "Failed to create repomd.xml"
 
 # Sign repository (repomd.xml files)
-gpg2 --default-key $GPG_KEY_ID --batch --yes --no-tty --armor --digest-algo SHA256 --detach-sign $cent6/x86_64/repodata/repomd.xml
+gpg2 --default-key $GPG_KEY_ID --batch --yes --no-tty --armor --digest-algo SHA256 --detach-sign $cent/repodata/repomd.xml || \
+    die "Failed to sign repomd.xml file."
 
 # Generate 'mirrors'
-mirrors_content="http://$bucket.s3.amazonaws.com/repo/assurio-snap/$branch/rpm/CentOS/\$releasever/\$basearch"
-for t in '' 'Client' 'Server' 'Workstation'; do
-	echo $mirrors_content > $out/CentOS/mirrors/6$t
+mirrors_content="$repo_link/$branch/rpm/CentOS/\$releasever/\$basearch"
+for kind in '' 'Client' 'Server' 'Workstation'; do
+    echo $mirrors_content > $mirrors/$dist_ver$kind
 done
 
 # Show repo's tree to the log
