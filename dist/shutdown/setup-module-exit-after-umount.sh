b46b8d17b2ae72d90c5bbf51bc3e7104bed07b61
Michal <mswitala@datto.com>
2023-05-15 12:28:05 +0200

    removing debug flag

--- dist/shutdown/setup-module-exit-after-umount.sh
+++ dist/shutdown/setup-module-exit-after-umount.sh
@@ -3,7 +3,7 @@
 # After introducing changes with ftrace there was an issue with calling module-exit during shutdown.
 # It looked like it is not called. The aim of this script is to call rmmod manually after unmounting the filesystem.
 
-cp /bin/call-rmmod-after-umount.sh /bin/call-rmmod-after-umount.sh
+cp bin/call-rmmod-after-umount.sh /bin/call-rmmod-after-umount.sh
 mv /bin/umount /bin/umount.real
 chmod +x /bin/call-rmmod-after-umount.sh
 ln -s /bin/call-rmmod-after-umount.sh /bin/umount
