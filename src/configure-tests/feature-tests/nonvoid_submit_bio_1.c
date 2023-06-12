198fc1f819315a1a203c9214611c60d5e899a646
natalia-zelazna <129159148+natalia-zelazna@users.noreply.github.com>
2023-06-12 11:03:39 +0200

    V5 19 compiles (#305)
    * Fixes for RHEL (and RHEL-like) 9.1 kernel backports
    
    * WIP fixing rhel9.1 backports
    
    * compiled v5.19
    
    * fix backwards compatibility1
    
    * review
    
    ---------
    
    Co-authored-by: Dakota Williams <drwilliams@datto.com>
    Co-authored-by: Natalia Zelazna <natalia.zelazna@datto.lan>

---
+++ src/configure-tests/feature-tests/nonvoid_submit_bio_1.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+/*
+ * Copyright (C) 2015-2023 Datto Inc.
+ */
+
+#include "includes.h"
+
+MODULE_LICENSE("GPL");
+
+static inline void dummy(void){
+	int ret = (int)submit_bio(NULL);
+}
