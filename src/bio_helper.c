f99eea256772d2c61a6d4e212b63224a275dca47
natalia-zelazna <129159148+natalia-zelazna@users.noreply.github.com>
2024-01-09 12:30:33 +0100

    Revert "Bio set op attrs removal (#337)"
    This reverts commit db3dab4eb9bedb9678e75e5f33da572db695500b.

--- src/bio_helper.c
+++ src/bio_helper.c
@@ -171,7 +171,6 @@ typedef enum req_op req_op_t;
 typedef enum req_opf req_op_t;
 #endif
 
-
 /**
  * dattobd_set_bio_ops() - Sets the op and its flags.
  *
@@ -182,7 +181,7 @@ typedef enum req_opf req_op_t;
 void dattobd_set_bio_ops(struct bio *bio, req_op_t op, unsigned op_flags)
 {
         bio->bi_opf = 0;
-        bio->bi_opf = op | op_flags;
+        bio_set_op_attrs(bio, op, op_flags);
 }
 
 /**
