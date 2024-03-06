dd7c8c45c70a7a6cbf4e0cb305d448c66a796bb3
Swistusmen <michal76aw@gmail.com>
2024-03-06 09:22:29 +0100

    fix

--- src/blkdev.h
+++ src/blkdev.h
@@ -55,7 +55,7 @@ struct block_device *blkdev_get_by_path(const char *path, fmode_t mode,
 #define dattobd_get_super(bdev) get_super(bdev)
 #define dattobd_drop_super(sb) drop_super(sb)
 #else 
-struct super_block* dattobd_get_active_super(struct block_device*)
+struct super_block* dattobd_get_active_super(struct block_device*);
 #define dattobd_get_super(bdev) dattobd_get_active_super(bdev)
 #define dattobd_drop_super(sb)
 #endif
