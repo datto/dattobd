7e40272e50f25f41c490780b8c569e9aa2a12cc4
Aliaksandr Shcherba <alexscherba16@gmail.com>
2024-06-13 12:36:32 -0400

    Use snap_open/release with gendisk and blk_mode type

--- src/snap_ops.c
+++ src/snap_ops.c
@@ -64,6 +64,17 @@ static int snap_release(struct gendisk *gd, fmode_t mode)
 {
         return __tracer_close(gd->private_data);
 }
+#elif defined HAVE_BDOPS_OPEN_WITH_BLK_MODE
+static int snap_open(struct gendisk *gd, blk_mode_t mode)
+{
+        (void)mode;
+        return __tracer_open(gd->private_data);
+}
+
+static void snap_release(struct gendisk *gd)
+{
+        __tracer_close(gd->private_data);
+}
 #else
 static int snap_open(struct block_device *bdev, fmode_t mode)
 {
